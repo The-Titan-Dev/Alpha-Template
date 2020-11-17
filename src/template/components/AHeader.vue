685a84aab1cb67107d0e990a961236fa63ef458b
Ariel G. Quindoza Jr <quindozajrariel@gmail.com>
2020-11-17 12:20:25 +0800

    add: template setup

---
+++ src/template/components/AHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+    <div class=""></div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "AHeader",
+};
+</script>
+
+<style lang="scss">
+@import "../../assets/sass/imports/general";
+@import "../../assets/sass/alpha/header.scss";
+</style>
