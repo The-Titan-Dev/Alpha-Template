3d2b71d95a4bb2d485d5f3ee3f48098a4cb23579
Gernale <jefferson.gernale@fujitsu.com>
2020-11-23 08:10:23 +0800

    [AButton] Initial - unfinished

---
+++ src/template/components/Button/AButton.vue
@@ -0,0 +1,70 @@
+<template>
+    <button @click = "doMethod" class="abtn" :class="'abtn--'+size+ ' abtn--'+atype">
+        <slot></slot>    
+    </button>    
+</template>
+
+
+<script>
+export default {
+    name : 'AButton',
+    props : {
+        method_name : {
+            type : String,
+            default : 'false'
+        },
+        size : {
+            type : String,
+            default : 'false'
+        },
+        atype : {
+            type : String,
+            default : 'false'
+        }
+    },
+    methods : {
+        doMethod() {
+            this.$parent[this.method_name]();
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../../../assets/sass/imports/general";
+
+.abtn {
+    border-radius : 5px;
+    border-style : solid;
+    font-size: 14px;
+    margin: 4px;
+    padding: 4px 14px;
+    
+    &--sm
+    {
+        font-size: 12px;
+        padding: 2px 7px;
+    }
+
+    &--md
+    {
+        font-size: 14px;
+        padding: 4px 14px;
+    }
+
+    &--lg
+    {
+        font-size: 20px;
+        padding: 5px 20px;
+    }
+
+    &--primary
+    {
+        background: $primary;
+        border-color: rgb(224, 224, 224);
+        color: rgb(221, 220, 220);
+
+    }
+}
+
+</style>
--- src/template/layout/TheContainer.vue
+++ src/template/layout/TheContainer.vue
@@ -1,16 +1,29 @@
 <template>
     <div>
         <TheHeader/>
+        <div style = "text-align: center; margin-top: 10px">
+            <AButton method_name = "doSomething">NORMAL</AButton>
+            <AButton method_name = "doSomething" size="sm">SMALL</AButton>
+            <AButton method_name = "doSomething" size="md">MEDIUM</AButton>
+            <AButton method_name = "doSomething" size="lg" atype="primary">LARGE</AButton>
+        </div>
         <router-view/>
     </div>
 </template>
 
 <script>
 import TheHeader from './TheHeader.vue'
+import AButton from '../components/Button/AButton.vue'
 export default {
     name:'TheContainer',
     components:{
         TheHeader,
+        AButton,
+    },
+    methods : {
+        doSomething(){
+            alert('Hello World')
+        }
     }
 }
 </script>
