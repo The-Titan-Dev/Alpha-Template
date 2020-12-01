a45d273378582653f4f381d189fe34bb158e839c
Ariel G. Quindoza Jr <quindozajrariel@gmail.com>
2020-12-01 08:50:09 +0800

    add: container template

--- src/template/layout/TheContainer.vue
+++ src/template/layout/TheContainer.vue
@@ -1,24 +1,37 @@
 <template>
-    <div>
-        <TheHeader></TheHeader>
-        <TheSidebar></TheSidebar>
-        <TheAside></TheAside>
-        <router-view/>
+  <div>
+    <TheHeader></TheHeader>
+    <TheSidebar></TheSidebar>
+    <TheAside></TheAside>
+    <div class="AlphaContainer">
+      <router-view />
     </div>
+    <TheFooter></TheFooter>
+  </div>
 </template>
 
 <script>
-import TheHeader from './TheHeader.vue'
-import TheSidebar from './TheSidebar.vue'
-import TheAside from './TheAside.vue'
+import TheHeader from "./TheHeader.vue";
+import TheSidebar from "./TheSidebar.vue";
+import TheAside from "./TheAside.vue";
+import TheFooter from "./TheFooter.vue";
 export default {
-    name:'TheContainer',
-    components:{
-        TheHeader,
-        TheSidebar,
-        TheAside,
-    }
-}
+  name: "TheContainer",
+  components: {
+    TheHeader,
+    TheSidebar,
+    TheAside,
+    TheFooter,
+  },
+};
 </script>
 
-<style></style>
+<style  lang="scss">
+@import "../../assets/sass/imports/general";
+
+.AlphaContainer{
+    width:100%;
+    min-height:100vh;
+    padding:100px 30px 30px 30px;
+}
+</style>
